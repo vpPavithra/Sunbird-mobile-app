2ab44c40f767c1c2d93fe541f0923182bad26681
vpPavithra <pavithra.prakash@tarento.com>
2023-04-05 12:17:53 +0530

    ng 12 migration

--- build.sh
+++ build.sh
@@ -50,9 +50,9 @@ done
 rm -rf platforms
 #Temporary Workaround to generate build as webpack was complaining of Heap Space
 #need to inspect on webpack dependdencies at the earliest
-NODE_OPTIONS=--max-old-space-size=4096 ionic cordova platforms add android@10.1.2
+NODE_OPTIONS=--max-old-space-size=8192 ionic cordova platforms add android@10.1.2
 
-NODE_OPTIONS=--max-old-space-size=6144 ionic cordova build android --prod --release --verbose --buildConfig ./buildConfig/build.json
+NODE_OPTIONS=--max-old-space-size=8192 ionic cordova build android production --release --verbose --buildConfig ./buildConfig/build.json
 
 if [ -n "$angularConfiguration" ]; then
   echo "$angularConfiguration"
